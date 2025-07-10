80f2b1faeda266e647776c072a2b4f56fa6746b6
Alan Malette <blsub6@gmail.com>
2025-07-10 01:33:41 -0700

    changed provider and updated

--- main.tf
+++ main.tf
@@ -1,26 +1,108 @@
-resource "proxmox_vm_qemu" "ubuntu_server" {
-  name        = "ubuntu-server"
-  target_node = "host"  # Your Proxmox node name
-  clone       = "UbuntuServerTemplate"  # The name of your cloud-init template
+resource "proxmox_virtual_environment_vm" "ansible" {
+  name       = "ansible"
+  node_name  = "host"                      
+  vm_id      = 109                        
+  clone {
+    vm_id = 203              
+    full  = true
+  }
 
-  os_type = "cloud-init"
+  cpu {
+    cores = 2
+  }
 
-  cores   = 2
-  memory  = 2048
-  scsihw  = "virtio-scsi-pci"
+  memory {
+    dedicated = 2048
+  }
 
   disk {
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "10G"
+    datastore_id = "HDD"            
+    file_format  = "qcow2"
+    interface    = "scsi0"
+    size         = 8
   }
 
-  network {
-    model  = "virtio"
+  network_device {
     bridge = "vmbr1"
+    model  = "virtio"
   }
 
-  ipconfig0 = "ip=dhcp"
+  agent {
+    enabled = true
+  }
 
-  sshkeys = file("~/.ssh/id_rsa.pub")
+  boot_order = ["scsi0"]
+  started    = true
+
+  initialization {
+    ip_config {
+     ipv4 {
+        address = "192.168.20.5/24"
+        gateway = "192.168.20.1"
+      }
+    }
+
+    dns {
+      servers = ["8.8.8.8", "8.8.4.4"]
+    }
+
+    user_account {
+      username = "user"
+      keys  = [var.ssh_key]
+    }
+  }
+}
+resource "proxmox_virtual_environment_vm" "nginx" {
+  name       = "nginx"
+  node_name  = "host"                      
+  vm_id      = 110                      
+  clone {
+    vm_id = 203              
+    full  = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "HDD"            
+    file_format  = "qcow2"
+    interface    = "scsi0"
+    size         = 8
+  }
+
+  network_device {
+    bridge = "vmbr1"
+    model  = "virtio"
+  }
+
+  agent {
+    enabled = true
+  }
+
+  boot_order = ["scsi0"]
+  started    = true
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "192.168.20.6/24"
+        gateway = "192.168.20.1"
+      }
+    }
+
+    dns {
+      servers = ["8.8.8.8", "8.8.4.4"]
+    }
+
+    user_account {
+      username = "user"
+      keys  = [var.ssh_key]
+    }
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,15 +1,32 @@
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source  = "Telmate/proxmox"
+#       version = "2.9.11"
+#     }
+#   }
+# }
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.11"
+      source  = "bpg/proxmox"
+      version = ">= 0.0.1"
     }
   }
 }
 
+
+# provider "proxmox" {
+#   pm_api_url      = "https://192.168.2.200:8006/api2/json"
+#   pm_user         = "root@pam"
+#   pm_password     = "Hurried-Remodeler-Labrador1"
+#   pm_tls_insecure = true
+# }
 provider "proxmox" {
-  pm_api_url      = "https://192.168.2.200:8006/api2/json"
-  pm_user         = "root@pam"
-  pm_password     = "Hurried-Remodeler-Labrador1"
-  pm_tls_insecure = true
+  endpoint = "https://192.168.2.200:8006/api2/json"
+  insecure = true
+
+  username = "root@pam"
+  password = "Hurried-Remodeler-Labrador1"
+
 }
